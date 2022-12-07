7a3bb2d85fd4b77d17650aa570f8a39140084337
asdcloud <asdcloud01@gmail.com>
2022-12-07 18:32:42 +0800

    enmey

---
+++ Practice_Platform_Game01/Assets/script/enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    
+    public float speed;
+    private Transform playerPos;
+
+
+
+    void Start() {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+}
