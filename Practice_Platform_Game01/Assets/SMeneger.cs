49079aecee2a28ad71a2c89d561ecfae95cc225f
bing-jun <jason46918577@gmail.com>
2022-12-15 01:59:23 +0800

    UI(2)

---
+++ Practice_Platform_Game01/Assets/SMeneger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SMeneger : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        //呼叫第一場景
+        SceneManager.LoadScene("Sample Scene");
+    }
+
+    public void QuitGame()
+    {
+        //離開遊戲
+        Application.Quit();
+    }
+}
