c34e2944c7195104c938bcf3d9fd74249ff1fd95
asdcloud <asdcloud01@gmail.com>
2022-12-18 17:40:48 +0800

    UI and bullet effect

--- Practice_Platform_Game01/Assets/script/bullet.cs
+++ Practice_Platform_Game01/Assets/script/bullet.cs
@@ -9,7 +9,7 @@ public class bullet : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision) {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 0.1f);
+        Destroy(effect, 0.3f);
         Destroy(gameObject);
     }
 }
